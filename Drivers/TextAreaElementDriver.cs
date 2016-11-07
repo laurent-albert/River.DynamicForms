8ba99937349a9934d496b21680407d692b800248
ramiassi <ramiassi@gmail.com>
2016-11-07 12:30:26 +0300

    Update TextAreaElementDriver.cs

--- Drivers/TextAreaElementDriver.cs
+++ Drivers/TextAreaElementDriver.cs
@@ -4,6 +4,7 @@ using Orchard.Forms.Services;
 using Orchard.Layouts.Framework.Display;
 using Orchard.Layouts.Framework.Drivers;
 using Orchard.Layouts.Helpers;
+using Orchard.Layouts.Services;
 using Orchard.Tokens;
 using DescribeContext = Orchard.Forms.Services.DescribeContext;
 
@@ -12,7 +13,7 @@ namespace River.DynamicForms.Drivers
     public class TextAreaElementDriver : FormsElementDriver<TextArea>
     {
         private readonly ITokenizer _tokenizer;
-        public TextAreaElementDriver(IFormManager formManager, ITokenizer tokenizer) : base(formManager)
+        public TextAreaElementDriver(IFormsBasedElementServices formManager, ITokenizer tokenizer) : base(formManager)
         {
             _tokenizer = tokenizer;
         }
@@ -40,7 +41,7 @@ namespace River.DynamicForms.Drivers
             });
         }
 
-        protected override void OnDisplaying(TextArea element, ElementDisplayContext context)
+        protected override void OnDisplaying(TextArea element, ElementDisplayingContext context)
         {
             context.ElementShape.ProcessedName = _tokenizer.Replace(element.Name, context.GetTokenData());
             context.ElementShape.ProcessedLabel = _tokenizer.Replace(element.Label, context.GetTokenData());
